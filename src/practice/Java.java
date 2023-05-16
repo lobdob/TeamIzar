7faf172be733845c6347e70ac735f9be7bca3492
Voloshyn9 <maksymvoloshyn5@gmail.com>
2023-05-15 21:10:22 -0400

    created new methods

---
+++ src/practice/Java.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Java {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public void method1(){
+
+    }
+
+    public void method2(){
+
+    }
+
+
+}
